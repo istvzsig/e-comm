619b8a21ddb939c3748f6f695bd86c6affdc2c3f
istvzsig <istvanzsigmond@aol.com>
2024-02-24 21:26:58 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1 className="text-3xl font-bold underline">Hello world</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
