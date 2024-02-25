20b71a58cb8738e6703cffce6f67d204d287073d
istvzsig <istvanzsigmond@aol.com>
2024-02-25 13:18:00 +0100

    update Slider component

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -18,28 +18,29 @@ class Slider extends React.Component {
       this.props.dispatch(stopAnimation(nextIndex));
       // Trigger the animation again after a short delay
       setTimeout(this.animate, 4000);
-    }, 1000);
+    }, 4000);
   }
 
   render() {
     const { slideIndex } = this.props;
     return (
-      <div className="relative pb-4">
-        <div className="z-0">
+      <div className="relative bg-[#ffa555] h-[500px] overflow-hidden">
+        <div>
           {dummyData.map((item, index) => (
             <div
               key={index}
-              className={`absolute z-0 opacity-${item.id === slideIndex ? '100' : '0'} duration-700 ease-in-out scale-${item.id === slideIndex ? '100' : '105'}`}>
-              <div>
-                <img src={item.img} alt="space" />
+              className={`absolute opacity-${item.id === slideIndex ? '100' : '0'} duration-700 ease-in-out scale-${item.id === slideIndex ? '100' : '105'}`}>
+              <div className="">
+                <img className="align-middle" src={item.img} alt="space" />
               </div>
-              <div>
-                <h3>{item.text}</h3>
+              <div className="absolute top-[50%] left-[50%] text-white font-bold text-[15vw] text-center translate-x-[-50%] translate-y-[-50%]">
+                <h3>{item.text.toUpperCase()}</h3>
               </div>
-              <div></div>
             </div>
           ))}
         </div>
+
+
       </div>
     );
   }
