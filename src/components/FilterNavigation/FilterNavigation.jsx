5626005f874027e3ebec6e69576dfbbeb52cd921
istvzsig <istvanzsigmond@aol.com>
2024-02-25 19:36:37 +0100

    add active style to the buttons + prettier code

--- src/components/FilterNavigation/FilterNavigation.jsx
+++ src/components/FilterNavigation/FilterNavigation.jsx
@@ -1,50 +1,89 @@
 import React from "react";
-import featuredSpaceSuit from '../../assets/img/featured-space-suit.png';
-import { filterProducts, resetFilter } from "../../features/slices/productsSlice";
-import { connect } from 'react-redux';
+import featuredSpaceSuit from "../../assets/img/featured-space-suit.png";
+import { connect } from "react-redux";
+import {
+  filterProducts,
+  resetFilter,
+} from "../../features/slices/productsSlice";
 
 class FilterNavigation extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      activeItem: "All",
+    };
+  }
   filterProductsByType = (itemType) => {
     this.props.dispatch(filterProducts(itemType));
-  }
+    this.setState({ activeItem: itemType });
+  };
 
   showAllProducts = () => {
-    this.props.dispatch(resetFilter()); // Dispatch resetFilter action
-  }
+    this.props.dispatch(resetFilter());
+    this.setState({ activeItem: "All" });
+  };
 
   render() {
-    const itemTypes = ['All', 'Spacehips', 'Planets', 'Suits', 'Badges', 'Boots', 'Helmets', 'Gloves', 'Accessories'];
+    const { activeItem } = this.state;
+
+    const itemTypes = [
+      "All",
+      "Spacehips",
+      "Planets",
+      "Suits",
+      "Badges",
+      "Boots",
+      "Helmets",
+      "Gloves",
+      "Accessories",
+    ];
     return (
       <div className="overflow-hidden bg-white relative z-1">
-        <h1 className="text-black text-center font-bold py-12 text-8xl">PRODUCTS LIST</h1>
+        <h1 className="text-black text-center font-bold py-12 text-8xl">
+          PRODUCTS LIST
+        </h1>
         <div className="text-center py-3 skew-x-[-12deg]">
           <h1 className="bg-black text-white inline p-3 px-20 font-bold text-3xl">
             SALES UP TO 50%
           </h1>
         </div>
         <div className="flex justify-center py-10 pb-20 mb-0 overflow-hidden">
-          <img className="rounded-xl md:w-[40%]  sm:w-[80%] shadow-[0_35px_60px_-15px_rgba(0,0,0,0.5)] " src={featuredSpaceSuit} alt="featured-img" />
+          <img
+            className="rounded-xl md:w-[40%]  sm:w-[80%] shadow-[0_35px_60px_-15px_rgba(0,0,0,0.5)] "
+            src={featuredSpaceSuit}
+            alt="featured-img"
+          />
         </div>
-        <h1 className="text-black text-center font-bold pb-12 text-4xl">OUR PRODUCTS</h1>
+        <h1 className="text-black text-center font-bold pb-12 text-4xl">
+          OUR PRODUCTS
+        </h1>
         <div className="flex justify-center items-center py-8 px-8 overflow-scroll scrollbar-hide">
           {itemTypes.map((itemType, index) => {
-            return <div
-              key={index}
-              onClick={() => itemType === 'All' ? this.showAllProducts() : this.filterProductsByType(itemType)}
-              className="mr-8"
-            >
-              <button className="bg-none border-2 border-[#eee] text-[grey] px-8 py-4 rounded-lg text-[12px]">
-                {itemType.toUpperCase()}
-              </button>
-            </div>
+            return (
+              <div
+                key={index}
+                onClick={() =>
+                  itemType === "All"
+                    ? this.showAllProducts()
+                    : this.filterProductsByType(itemType)
+                }
+                className="mr-8"
+              >
+                <button
+                  className={`bg-none hover:bg-[blue] text-[grey] hover:text-[white] border-2 border-[#eee] px-4 py-2 rounded-lg text-[12px] transition ease-in-out ${
+                    activeItem === itemType ? "bg-[blue] text-[white]" : ""
+                  }`}
+                >
+                  {itemType.toUpperCase()}
+                </button>
+              </div>
+            );
           })}
         </div>
       </div>
-    )
+    );
   }
 }
 
-const mapStateToProps = state => ({ filteredProducts: state.products });
+const mapStateToProps = (state) => ({ filteredProducts: state.products });
 export default connect(mapStateToProps)(FilterNavigation);
-
-
--- src/components/ProductsList/ProductsList.jsx
+++ src/components/ProductsList/ProductsList.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import spaceSuit from '../../assets/img/featured-space-suit.png';
+import spaceSuit from "../../assets/img/featured-space-suit.png";
 import { connect, useSelector } from "react-redux";
-import { storeData } from '../../assets/data/storeData';
+import { storeData } from "../../assets/data/storeData";
 import {
   Card,
   CardHeader,
@@ -12,37 +12,49 @@ import {
 
 class ProductsList extends React.Component {
   render() {
-    console.log(this.props.filteredProducts)
+    console.log(this.props.filteredProducts);
     return (
       <div className="flex flex-col bg-white py-12 relative z-[-1]">
         <div className="flex flex-wrap justify-center">
           {this.props.filteredProducts.map((item, index) => {
-            return <Card key={index} className="mt-12 w-96 mx-6 cursor-poinder grayscale-0 hover:grayscale">
-              <CardHeader className="relative h-56">
-                <img
-                  src={item.img}
-                  alt="card-image"
-                />
-              </CardHeader>
-              <CardBody>
-                <Typography variant="h5" color="blue-gray" className="mb-2 flex justify-between items-center">
-                  {item.name}
-                  <Typography className="font-bold italic text-[12px] mb-2 text-white p-2 rounded-lg bg-[rgba(0,0,255,1)]">{item.type}</Typography>
-                </Typography>
+            return (
+              <Card
+                key={index}
+                className="mt-12 w-96 mx-6 cursor-poinder grayscale-0 hover:grayscale"
+              >
+                <CardHeader className="relative h-56">
+                  <img src={item.img} alt="card-image" />
+                </CardHeader>
+                <CardBody>
+                  <Typography
+                    variant="h5"
+                    color="blue-gray"
+                    className="mb-2 flex justify-between items-center"
+                  >
+                    {item.name}
+                    <Typography className="font-bold italic text-[12px] mb-2 text-white p-2 rounded-lg bg-[rgba(0,0,255,1)]">
+                      {item.type}
+                    </Typography>
+                  </Typography>
 
-                <Typography>{item.text}</Typography>
-              </CardBody>
-              <CardFooter className="pt-0 flex">
-                <h1 className="text-black text-left font-bold text-4xl">${item.price}</h1>
-                <p className="text-[10px] text-black font-bold">USD</p>
-              </CardFooter>
-            </Card>
+                  <Typography>{item.text}</Typography>
+                </CardBody>
+                <CardFooter className="pt-0 flex">
+                  <h1 className="text-black text-left font-bold text-4xl">
+                    ${item.price}
+                  </h1>
+                  <p className="text-[10px] text-black font-bold">USD</p>
+                </CardFooter>
+              </Card>
+            );
           })}
         </div>
       </div>
-    )
+    );
   }
 }
 
-const mapStateToProps = state => ({ filteredProducts: state.products.filteredProducts });
+const mapStateToProps = (state) => ({
+  filteredProducts: state.products.filteredProducts,
+});
 export default connect(mapStateToProps)(ProductsList);
