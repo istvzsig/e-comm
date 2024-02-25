dea35057c3fed2ebc9813cfb8f65a752b4f0ab2c
istvzsig <istvanzsigmond@aol.com>
2024-02-25 15:17:19 +0100

    add productsSlice to store

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import sliderSlice from '../features/slices/sliderSlice';
+import productsSlice from "../features/slices/productsSlice";
 
 export const store = configureStore({
   reducer: {
     slider: sliderSlice,
+    products: productsSlice
   },
 });
