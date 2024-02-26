1e4cc08d3c72588643fb5fe796a69d89aa69905b
istvzsig <istvanzsigmond@aol.com>
2024-02-26 19:19:29 +0100

    refactor + create ProductCard component + update ProductsList component with ProductCard

---
+++ src/components/ProductCard/ProductCard.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Typography,
+} from "@material-tailwind/react";
+import { Link } from "react-router-dom";
+
+class ProductCard extends React.Component {
+  render() {
+    const { ...item } = this.props;
+    return (
+      <Card className="select-none mt-12 w-96 mx-6 cursor-poinder">
+        <CardHeader className="grayscale-0 hover:grayscale relative transition ease-in-out delay-150 ">
+          <img className="rounded" src={item.img} alt="card-image" />
+        </CardHeader>
+        <CardBody>
+          <Typography
+            variant="h5"
+            color="blue-gray"
+            className="mb-2 flex justify-between items-center"
+          >
+            {item.name}
+            <Typography className="font-bold italic text-[12px] mb-2 text-white p-2 rounded-lg bg-[rgba(0,0,255,1)]">
+              {item.type}
+            </Typography>
+          </Typography>
+          <Typography className="text-sm font-medium text-[rgba(0,0,0,.5)]">
+            {item.text}
+          </Typography>
+        </CardBody>
+        <CardFooter className="pt-0 flex justify-between items-center">
+          <h1 className="text-black text-left font-bold text-4xl">
+            {item.price !== "FREE" ? `$${item.price}` : item.price}
+            <span className="text-[10px] text-black font-bold self-start relative top-[-18px] left-[2px]">
+              {item.price == "FREE" ? "" : "USD"}
+            </span>
+          </h1>
+          <Link
+            to={`/product/${item.type.toLowerCase()}/${item.id}`}
+            onClick={() => this.showProduct(item.id)}
+          >
+            <p className="text-[12px] text-black font-bold">READ MORE</p>
+          </Link>
+        </CardFooter>
+      </Card>
+    );
+  }
+}
+
+export default ProductCard;
--- src/components/ProductsList/ProductsList.jsx
+++ src/components/ProductsList/ProductsList.jsx
@@ -1,14 +1,7 @@
 import React from "react";
+import ProductCard from "../ProductCard/ProductCard";
 import { connect } from "react-redux";
 import { getProduct } from "../../features/slices/productsSlice";
-import {
-  Card,
-  CardHeader,
-  CardBody,
-  CardFooter,
-  Typography,
-} from "@material-tailwind/react";
-import { Link } from "react-router-dom";
 
 class ProductsList extends React.Component {
   showProduct = (id) => {
@@ -20,42 +13,7 @@ class ProductsList extends React.Component {
       <div className="flex flex-col bg-white py-12 relative z-[0]">
         <div className="flex flex-wrap justify-center mt-12">
           {this.props.filteredProducts.map((item, index) => {
-            const { type, id } = item;
-
-            return (
-              <Link
-                key={index}
-                to={`/product/${type.toLowerCase()}/${id}`}
-                onClick={() => this.showProduct(id)}
-              >
-                <Card className="select-none mt-12 w-96 mx-6 cursor-poinder">
-                  <CardHeader className="grayscale-0 hover:grayscale relative transition ease-in-out delay-150 ">
-                    <img className="rounded" src={item.img} alt="card-image" />
-                  </CardHeader>
-                  <CardBody>
-                    <Typography
-                      variant="h5"
-                      color="blue-gray"
-                      className="mb-2 flex justify-between items-center"
-                    >
-                      {item.name}
-                      <Typography className="font-bold italic text-[12px] mb-2 text-white p-2 rounded-lg bg-[rgba(0,0,255,1)]">
-                        {item.type}
-                      </Typography>
-                    </Typography>
-                    <Typography className="text-sm font-medium text-[rgba(0,0,0,.5)]">
-                      {item.text}
-                    </Typography>
-                  </CardBody>
-                  <CardFooter className="pt-0 flex">
-                    <h1 className="text-black text-left font-bold text-4xl">
-                      ${item.price}
-                    </h1>
-                    <p className="text-[10px] text-black font-bold">USD</p>
-                  </CardFooter>
-                </Card>
-              </Link>
-            );
+            return <ProductCard key={index} {...item} />;
           })}
         </div>
       </div>
